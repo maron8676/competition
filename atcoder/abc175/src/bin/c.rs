7a4e0bed5e4dbba2e5de7c3ab7c86fd9c4153496
acro.fujiwara <fujiwara@acroquest.co.jp>
2020-08-22 15:58:41 +0900

    add abc175-c

--- atcoder/abc175/src/bin/c.rs
+++ atcoder/abc175/src/bin/c.rs
@@ -3,6 +3,21 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
-        n: usize,
+        x: i64,
+        k: i64,
+        d: i64
+    }
+
+    let abs_x = x.abs();
+    if abs_x / d >= k {
+        println!("{}", abs_x - d * k)
+    } else {
+        let is_even = (k - abs_x / d) % 2 == 0;
+        let remains = abs_x % d;
+        if is_even {
+            println!("{}", remains);
+        } else {
+            println!("{}", d - remains)
+        }
     }
 }
